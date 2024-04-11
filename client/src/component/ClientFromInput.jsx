29729e054da34b025da5448f1e316dbfe4bb34c6
Dhruvi Italiya <dhruvii@codesquote.com>
2024-04-11 13:13:11 +0530

    last Commit

--- client/src/component/ClientFromInput.jsx
+++ client/src/component/ClientFromInput.jsx
@@ -2,20 +2,15 @@ import { Input } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import '../style/ClientFromInput.css'
 
 function ClientFromInput() {
   const navigate = useNavigate()
 
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyPress);
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  }, [])
-
   const [FormData, setFormData] = useState({
     clientname: '',
     address: '',
+    mobile: "",
     sku: '',
   })
 
@@ -41,20 +36,35 @@ function ClientFromInput() {
   };
   return (
     <>
-      <div style={{ width: '50%', margin: '0 auto' }}>
-        <div style={{ margin: '10px 0' }}>
-          <Input placeholder="Name" name='clientname' value={FormData.clientname} onChange={handalChnage} />
-        </div>
-        <div style={{ margin: '10px 0' }}>
-          <TextArea rows={4} name='address' placeholder="Enter Address" value={FormData.address} onChange={handalChnage} />
-        </div>
-        <div style={{ margin: '10px 0' }}>
-          <Input placeholder="Enter SKU" name='sku' value={FormData.sku} onChange={handalChnage} />
-        </div>
-        <div className="div">
-          <button type='button' onClick={previewPage}>
-            Preview
-          </button>
+
+      <div className="main-div1">
+        <h1 className='heading'>Welcome To Blink Fashion</h1>
+        <div className="container1">
+          <div className="parent1">
+            <form>
+              <div className="input-div">
+                <label className='labelforfrom'>Client Name:-</label>
+                <Input placeholder="Name" name='clientname' value={FormData.clientname} onChange={handalChnage} onKeyDown={handleKeyPress} />
+              </div>
+              <div className="input-div">
+                <label className='labelforfrom'>Client Address:-</label>
+                <TextArea rows={4} name='address' placeholder="Enter Address" value={FormData.address} onChange={handalChnage} onKeyDown={handleKeyPress}  />
+              </div>
+              <div className="input-div">
+                <label className='labelforfrom'>Client Mobile No:-</label>
+                <Input placeholder="Enter Mobile Number" type='number' name='mobile' value={FormData.mobile} onChange={handalChnage}  onKeyDown={handleKeyPress} />
+              </div>
+              <div className="input-div">
+                <label className='labelforfrom'>Product SKU:-</label>
+                <Input placeholder="Enter SKU" name='sku' value={FormData.sku} onChange={handalChnage} onKeyDown={handleKeyPress} />
+              </div>
+            </form>
+          </div>
+          <div className="input-div">
+            <button type='button' onClick={previewPage} className='printbtn'>
+              Preview
+            </button>
+          </div>
         </div>
       </div>
     </>
--- client/src/component/PrintUl.jsx
+++ client/src/component/PrintUl.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import '../style/PrintUl.css'
+import instagram from '../image/instagram.svg'
+import whatsapp from '../image/whatsapp.svg'
+import moment from 'moment';
 
 function PrintUl() {
     const [obj, setobj] = useState({})
-    const [printButtonClicked, setPrintButtonClicked] = useState(false);
     useEffect(() => {
         setobj(JSON.parse(localStorage.getItem("ClientDetail")))
         document.addEventListener('keydown', handleKeyPress);
@@ -17,19 +20,56 @@ function PrintUl() {
         }
     };
 
-    const printData = (e) => {
-        setPrintButtonClicked(true);
-        setTimeout(() => {
-            window.print()
-        }, 300);
-        setTimeout(() => {
-            setPrintButtonClicked(false)
-        }, 300);
+    const printData = () => {
+        window.print()
     }
     return (
         <>
-            <div>name:-{obj.clientname}</div>
-            {!printButtonClicked && <button onClick={printData}>Print</button>}
+            <div className="main-div">
+                <div className="container">
+                    <div className="parent">
+                        <div className="demo">
+                            <div className="child">
+                                BLINK_FASHION
+                            </div>
+                            <img src={require('../image/bd928175-88da-4acf-9d46-93ded5490f23.jpg')} className="img" alt="" />
+                        </div>
+                        <div className="child-2">
+                            <div>
+                                <span>Name:-</span><span>{obj.clientname}</span>
+                            </div>
+                            <div className='address'>
+                                <span>Address:-</span>{obj.address}
+                            </div>
+                        </div>
+                        <div className="child-5">
+                            {obj.mobile}
+                        </div>
+                        <div className="child-6">
+                            <p>Date:-{moment().format('DD/MM/YYYY')}</p>
+                            <p>SKU-{obj.sku}</p>
+                        </div>
+                        <div className="child-3">
+                            thank you for shopping
+                        </div>
+                        <div className="child-4">
+                            <div className="abc" style={{ borderRight: "2px solid black" }}>
+                                <div className='image-div'><img src={instagram} alt="" style={{ marginRight: "5px" }} /></div>
+                                <div>@blinkfashion45</div>
+                            </div>
+                            <div className="abc">
+                                <div className='image-div'> <img src={whatsapp} alt="" style={{ marginRight: '5px' }} /></div>
+                                <div>@blinkfashion45</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="btn-div">
+                    <button onClick={printData} className='printbtn'>Print</button>
+                </div>
+            </div>
+
+
         </>
     )
 }
